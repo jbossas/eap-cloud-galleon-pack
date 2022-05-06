67e050552bd16f961d3929e1fac1409fc6976af2
JF Denise <jdenise@redhat.com>
2022-05-06 11:45:31 +0200

    Initial commit for cloud fp content

---
+++ eap-cloud-galleon-pack/install-wildfly-cekit-modules.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+# Install launch scripts and galleon common content.
+SCRIPT_DIR=$(pwd -P)/$(dirname $0)
+tmp_dir="$SCRIPT_DIR/target/tmp"
+mkdir -p $tmp_dir
+resources_dir="$SCRIPT_DIR/target/resources"
+pushd "$tmp_dir" > /dev/null
+  echo "git clone -b $WILDFLY_CEKIT_TAG http://github.com/$WILDFLY_CEKIT_FORK/wildfly-cekit-modules"
+  git clone -b $WILDFLY_CEKIT_TAG http://github.com/$WILDFLY_CEKIT_FORK/wildfly-cekit-modules
+  pushd wildfly-cekit-modules/export
+    bash ./export-cekit-modules.sh "$resources_dir" "$SCRIPT_DIR/wf-cekit-modules-fp-content-list.txt" "$SCRIPT_DIR/wf-cekit-modules-launch-list.txt" "$SCRIPT_DIR/wf-cekit-modules-launch-config-list.txt"
+  popd > /dev/null
+popd > /dev/null
+
